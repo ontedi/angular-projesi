c0768bd96a3ed16c155498946f8414527cdd426b
ontedi <universityofcu@gmail.com>
2022-01-08 00:11:48 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-kapsul',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Projesi Başlık';
+}
