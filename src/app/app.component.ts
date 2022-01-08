e9746ae66e9405380d9a8f01ffb3e45e2a68a6a5
ontedi <universityofcu@gmail.com>
2022-01-08 14:18:18 +0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-kapsul',
+  selector: '#ng-kapsul',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
